041c20d8c9925f2e5e4f88cb07bf4991dc141fc1
arif3ana <ariftriana28@gmail.com>
2022-10-25 14:34:34 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function() {
+	hamburger.classList.toggle("is-active");
+});
+AOS.init({
+	once: true
+});
+gsap.registerPlugin(TextPlugin);
+gsap.to('.tagline', {duration: 3, delay: 1.5, text: 'i build and develop websites'});
+gsap.from('.main-text', {duration: 1, y: -100, opacity: 0, ease: 'bounce'});
